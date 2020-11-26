4dd76752ec39d5391553971e3022c629d2820452
juliamattson <julia.mattson@medieinstitutet.se>
2020-11-26 17:04:04 +0100

    settings

---
+++ Wooconfig.js
@@ -0,0 +1,7 @@
+const WooConfig = {
+    siteUrl: 'http://hjartanavguld.se/',
+    consumerKey: 'ck_f2d6721af3cf16085efa560c43d8bfeb6f2b3ff2',
+    consumerSecret: 'cs_d1fc0178935d5ecb830258498d24a0e446dde8c0',
+};
+
+module.exports = WooConfig;
--- server.js
+++ server.js
@@ -1,23 +1,45 @@
 const next = require('next');
 const express = require('express');
+const WooConfig = require('./WooConfig');
+
+const WooCommerceAPI = require('woocommerce-api');
+
+const WooCommerce = new WooCommerceAPI({
+    url: WooConfig.siteUrl,
+    consumerKey: WooConfig.consumerKey,
+    consumerSecret: WooConfig.consumerSecret,
+    wpAPI: true,
+    version: 'wc/v1'
+});
+
 const port = 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
+
 app.prepare()
     .then(() => {
         const server = express();
+
+        server.get('/getProducts', (req, response) => {
+            WooCommerce.get('products', function (err, data, res) {
+                response.json(JSON.parse(res));
+            });
+        });
+
         server.get('*', (req, res) => {
             return handle(req, res);
-        })
+        });
+
         server.listen(port, err => {
             if (err) {
                 throw err;
             }
-            console.log(`Site is running on port ${port}`);
+            console.log(`Ready on ${port}`);
         })
+
     })
     .catch(ex => {
         console.error(ex.stack);
         process.exit(1);
-    }); 
+    });;
