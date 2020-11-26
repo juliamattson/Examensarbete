38b59673e94acf07479406a0736e4fda3890675b
juliamattson <julia.mattson@medieinstitutet.se>
2020-11-26 16:34:15 +0100

    Satt upp server

--- Components/Nav.js
+++ Components/Nav.js
@@ -1,26 +1,26 @@
 const Nav = () => {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Hjärtan av guld UF</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="#">Hjärtan av guld UF</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse" id="navbarColor03">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Hem
-          <span class="sr-only">(current)</span>
+            <div className="collapse navbar-collapse" id="navbarColor03">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Hem
+          <span className="sr-only">(current)</span>
                         </a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Produkter</a>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Produkter</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Om oss</a>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Om oss</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Kontakt</a>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Kontakt</a>
                     </li>
                 </ul>
             </div>
