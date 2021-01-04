c29e390e5b7a0db234c3eba2b83ad0767bccb317
fridaedda <frida.baldvinsdottir@gmail.com>
2021-01-04 17:33:32 +0100

    Lanserat wordpress på webben
    Lanserat wordpress på webben

--- Wooconfig.js
+++ Wooconfig.js
@@ -1,5 +1,5 @@
 const WooConfig = {
-    siteUrl: 'http://localhost/',
+    siteUrl: 'https://wp.hjartanavguld.se/',
     consumerKey: 'ck_f2d6721af3cf16085efa560c43d8bfeb6f2b3ff2',
     consumerSecret: 'cs_d1fc0178935d5ecb830258498d24a0e446dde8c0',
 };
