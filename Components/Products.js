31f44127d61de58f3a7513d847975208422b1ca4
juliamattson <julia.mattson@medieinstitutet.se>
2021-01-11 13:57:52 +0100

    Skapat symbol för lagerstatus, #11

--- Components/Products.js
+++ Components/Products.js
@@ -9,6 +9,12 @@ const Product = (props) => {
             <div className="card-body">
                 <h5 className="card-title">{product.price}</h5>
                 <AddToCartButton product={product} />
+                {product.purchasable && (
+                    <p className="stock-status available"><i className="fa fa-circle mr-1"></i> Tillgänglig</p>
+                )}
+                {!product.purchasable && (
+                    <p className="stock-status not-available"><i className="fa fa-circle mr-1"></i> Ej tillgänglig</p>
+                )}
             </div>
         </div>
     )
