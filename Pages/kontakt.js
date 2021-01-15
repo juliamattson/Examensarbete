ca5f0ebd2fda469c8762c573a4bd345def45ff08
juliamattson <julia.mattson@medieinstitutet.se>
2021-01-15 14:53:38 +0100

    GQL för kontaktsida påbörjad

--- Pages/kontakt.js
+++ Pages/kontakt.js
@@ -1,12 +1,30 @@
+import gql from "graphql-tag";
+import client from '../Components/ApolloClient';
 import Layout from "../Components/Layout";
 
-const Kontakt = () => {
+const CONTACT_QUERY = gql`query{
+    page(id:"cG9zdDoyMA==") {
+        title
+        content
+      }
+    }
+`;
+
+const Kontakt = (props) => {
+    const { page } = props;
     return (
         <Layout>
-            <div>Kontaktinfo</div>
-            <div>info@hjartanavguld.se</div>
+            <h1>{page.title}</h1>
+            <p>{page.content}</p>
         </Layout>
     )
+};
+
+Kontakt.getInitialProps = async () => {
+    const result = await client.query({ query: CONTACT_QUERY });
+    return {
+        page: result.data.page
+    }
 };
 
 export default Kontakt;
