d3123df8faea0d4339ba10d74c81f9d92f0753f5
juliamattson <julia.mattson@medieinstitutet.se>
2021-01-19 11:27:09 +0100

    ändrat mappnamn

--- pagestwo/cart.js
+++ pagestwo/cart.js
@@ -2,6 +2,7 @@ import CartItemsContainer from "../Components/Cart/CartPage/CartItemsContainer";
 import Layout from "../Components/Layout";
 
 //ändrat i filen
+/* hej */
 const Cart = () => {
     return (
         <Layout>
