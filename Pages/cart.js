47adb415b8aaa7434d5bf2b9678b082859912465
fridaedda <frida.baldvinsdottir@gmail.com>
2021-01-19 11:09:15 +0100

    bl

--- Pages/cart.js
+++ Pages/cart.js
@@ -1,6 +1,7 @@
 import CartItemsContainer from "../Components/Cart/CartPage/CartItemsContainer";
 import Layout from "../Components/Layout";
 
+//ändrat i filen
 const Cart = () => {
     return (
         <Layout>
