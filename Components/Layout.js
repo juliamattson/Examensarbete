b58be9ebd51747ab604decb113530bdd6249312e
juliamattson <julia.mattson@medieinstitutet.se>
2021-02-01 10:04:53 +0100

    Added favicon

--- Components/Layout.js
+++ Components/Layout.js
@@ -9,6 +9,7 @@ const Layout = (props) => {
         <AppProvider>
             <div>
                 <Head>
+                    <link rel="icon" type="image/png" sizes="32x32" href="https://wp.hjartanavguld.se/wp-content/uploads/2021/01/cropped-logo_dark_charcoal.png" />
                     <title>Hjärtan av guld UF</title>
                     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
                     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
